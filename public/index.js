a56305db046683c62a54898ea15302525686702c
John <johnmsaylor@gmail.com>
2020-04-26 08:56:00 -0400

    manipulating x and y coord

--- public/index.js
+++ public/index.js
@@ -3,24 +3,37 @@ const canvas = document.querySelector("#canvas");
 // const cavas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-width = window.innerWidth;
-height = window.innerHeight;
+let x = 0,
+  y = 0;
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+let count = 0;
+const time = 100000;
 
 ctx.fillRect(0, 0, width, height);
 
-let count = 0;
-
-while (count < 100000) {
+while (count < time) {
   setTimeout(() => {
-    let x = getRandom(width);
-    let y = getRandom(height);
     let red = clamp(0, 150);
     let green = clamp(0, 50);
     let blue = clamp(200, 210);
     ctx.fillStyle = `rgba(${red},${green},${blue},0.35)`;
-    ctx.fillRect(x, y, 10, 10);
+    ctx.fillRect(wand(count, width).x, wand(count, width).y, 20, 20);
   }, 100);
   count += 1;
+}
+
+function wand(count, width) {
+  const delay = 10000;
+
+  const coord = {
+    x: 200,
+    y: getRandom(height),
+  };
+
+  return coord;
 }
 
 function clamp(min, max) {
